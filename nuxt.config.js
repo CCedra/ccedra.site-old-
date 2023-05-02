3f801ef2c4146de723c4026ee1aa25a017131bde
Ccedra <87731089+CCedra@users.noreply.github.com>
2023-05-02 11:56:28 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  server: {
+  /*server: {
     port: 8000, // порт, на котором будет работать сервер
     host: '0.0.0.0', // хост, на котором будет работать сервер
-  },
+  }, */
   ssr: false,
   target: 'server',
 
