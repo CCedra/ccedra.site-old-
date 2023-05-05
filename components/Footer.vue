4fd725d49510f3b5e994ac1bdac3659b0ec0ffcb
Ccedra <87731089+CCedra@users.noreply.github.com>
2023-05-05 10:11:14 +0300

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
-    <footer class="footer">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Наверх</a>
-        </p>
-        <span class="text-muted">Подвальчек.</span>
-      </div>
-    </footer>
-  </template>
-  
-  <script>
-  export default {
+  <footer class="footer">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Наверх</a>https://github.com/CCedra/ccedra.site/blob/master/components/Footer.vue
+      </p>
+      <span class="text-muted">Подвальчек.</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
   name: 'Footer'
-  }
-  </script>
-  
-  <style scoped>
-  
-  </style>
+}
+</script>
+
+<style scoped>
+
+</style>
